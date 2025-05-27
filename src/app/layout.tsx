ab42377b7a804d9bea02071fa9a049833b1de4b4
Avik Bhadra <biswanathbhadra1@gmail.com>
2025-05-27 09:50:33 +0000

    when i am switching from page to page , the switch has a delay ,fix that and add some effects when i am switching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         >
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <main className={cn("flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8", "animate-fadeIn")}>
               {children}
             </main>
             <Footer />
